73381299f837b4cae092bdd2272c7f47d5eef740
Frank Pagan <frank@cocreate.app>
2022-02-09 20:31:06 +0000

    fix: update CoCreate.config.js, bump @cocreate/docs version

---
+++ CoCreate.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    config: {
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_Id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
+    },
+    "sources": [
+        {
+            "entry": "./docs/index.html",
+            "collection": "files",
+            "document_id": "",
+            "key": "src",
+            "data": {
+                "name": "index.html",
+                "path": "/docs/socket-server/index.html",
+                "domains": [
+                    "cocreate.app",
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/socket-server",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "5ffbceb7f11d2d00103c4535"
+            }
+        }
+    ]
+}
