84d876c20a9dc0c7206a5371603d5d7fabe2b6ec
frankpagan <frank@cocreate.app>
2023-06-06 21:34:50 -0600

    add check-coc and add devDependency @cocreate/cli

--- demo/server.js
+++ demo/server.js
@@ -1,19 +1,24 @@
+'use strict';
 const express = require('express');
-const { createServer } = require('http');
-
-const SocketServer = require("../src/index")
-
-const ws_socket = new SocketServer("ws");
-const port = process.env.PORT || 3000;
+const cors = require('cors')
+const urlencoded = require('body-parser').urlencoded;
 
 const app = express();
+app.use(cors())
+app.use(urlencoded({ extended: false }));
 
+const socketServer = require("@cocreate/socket-server")
+const wsManager = new socketServer("ws");
+
+const { createServer } = require('http');
 const server = createServer(app);
-
 server.on('upgrade', function upgrade(request, socket, head) {
-    if (!ws_socket.handleUpgrade(request, socket, head)) {
+    if (!wsManager.handleUpgrade(request, socket, head)) {
         socket.destroy();
     }
 });
 
-server.listen(port);
+const components = require("./components")
+components.init(app, wsManager)
+
+server.listen(process.env.PORT || 3000);
