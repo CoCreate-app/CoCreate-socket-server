f16cea6ac65e41940d90a811f81eae1749b4a86e
frankpagan <frank@cocreate.app>
2023-10-03 20:30:12 -0600

    first draft of a socket mesh

---
+++ src/mesh.js
@@ -0,0 +1,59 @@
+const socket = require('@cocreate/socket-client')
+const crud = require('@cocreate/crud-client')
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+
+const organizations = {}
+const urls = new Map()
+const servers = {}
+
+function addServer(data) {
+    if (!servers[data.url])
+        servers[data.url] = { [data.serverId]: { [data.organization_id]: true } }
+    else
+        servers[data.url][data.serverId][data.organization_id] = true
+
+    // TODO: needs to get orgaizations activeRegions
+    socket.send({
+        method: 'region.added',
+        host: data.url,
+        activeRegions: '',
+        serverId: data.serverId,
+        organization_id: data.organization_id
+    })
+}
+
+function deleteServer(data) {
+    delete servers[data.url][data.serverId][data.organization_id]
+    if (!Object.keys(servers[data.url][data.serverId]).length)
+        delete servers[data.url][data.serverId]
+    if (!Object.keys(servers[data.url]).length) {
+        socket.send({
+            method: 'region.removed', // delete.region
+            host: data.url,
+            serverId: data.serverId,
+            organization_id: data.organization_id
+        })
+        socket.delete(data.url)
+    }
+
+}
+
+socket.listen('region.added', function (data) {
+    addServer(data)
+});
+
+socket.listen('region.removed', function (data) {
+    delete servers[data.url][data.serverId]
+    if (!Object.keys(servers[data.url]).length)
+        socket.delete(data.url)
+});
+
+eventEmitter.on('mesh.create', addServer);
+eventEmitter.on('mesh.delete', deleteServer);
+
+// eventEmitter.emit('customEvent', 'Hello, Node.js!');
+
+
+// let response = await crud.send(request)
