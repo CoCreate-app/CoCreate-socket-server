d0fffc777f099b242a0dccfe5ed6b8008d54dda2
frankpagan <frank@cocreate.app>
2023-10-21 02:09:20 -0600

    feat: ObjectId() returns an object containg the parts iof the _id along with a toString() function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const WebSocket = require('ws');
 const { URL } = require("url");
 const EventEmitter = require("events").EventEmitter;
 const uid = require('@cocreate/uuid')
-// const { ObjectId } = require('@cocreate/utils')
 const config = require('@cocreate/config')
 
 class SocketServer extends EventEmitter {
